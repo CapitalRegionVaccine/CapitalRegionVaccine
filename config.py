00fc4d429b0747dad2e227a160b850b7de6d649e
Capital Region Vaccine Finder <79386084+CapitalRegionVaccine@users.noreply.github.com>
2021-02-23 20:20:20 -0500

    Decouple hardcoded configuration data for better portability Create configuration file and remove hardcoded search data from main function.

---
+++ config.py
@@ -0,0 +1,7 @@
+## Configuration data
+config = {
+    "zipcode": "12110",
+    "nys_sites": ['SUNY Albany '],
+    "cvs_sites": ['WYNANTSKILL', 'SARATOGA SPRINGS', 'COLONIE', 'GLENVILLE', 'QUEENSBURY'],
+    "wal_location": '"latitude":42.7477661,"longitude":-73.760537'
+}
