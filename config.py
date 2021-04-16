e764002900d92cd1e7925e8b22254e1f1bd781d2
Capital Region Vaccine Finder <79386084+CapitalRegionVaccine@users.noreply.github.com>
2021-04-16 09:27:16 -0400

    Add Colonie Hannaford

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ config = {
     "alb_sites": 'Washington Avenue Armory',
     "qns_sites": 'Queensbury Aviation Mall',
     "cvs_sites": ['ALBANY', 'TROY', 'WYNANTSKILL', 'SARATOGA SPRINGS', 'COLONIE', 'GLENVILLE', 'QUEENSBURY', 'BURNT HILLS', 'GLENVILLE', 'LATHAM', 'RENSSELAER', 'SCHENECTADY'],
-    "han_sites": {'52080': 'Albany','51034': 'Albany','51005': 'Ballston Lake', '50992': 'Voorheesville', '50955': 'Ballston Spa', '51044': 'Wynantskill', '51045': 'Altamont', '51046': 'Ballston Spa', '51039': 'Latham', '51095': 'Troy', '51032': 'Clifton Park', '51112': 'Niskayuna', '51105': 'Valatie', '51043': 'Saratoga Springs', '51104': 'Amsterdam'},
+    "han_sites": {'52080': 'Albany','51034': 'Albany','51005': 'Ballston Lake', '50992': 'Voorheesville', '50955': 'Ballston Spa', '51044': 'Wynantskill', '51045': 'Altamont', '51046': 'Ballston Spa', '51039': 'Latham', '51095': 'Troy', '51032': 'Clifton Park', '51112': 'Niskayuna', '51105': 'Valatie', '51043': 'Saratoga Springs', '51104': 'Amsterdam', '51031': 'Colonie'},
     "wal_location": '"latitude":42.7477661,"longitude":-73.760537'
 }
